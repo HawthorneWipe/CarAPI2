7fec6941573034463c23127ef04db4d2793de228
Lukasz <lukasz.dziechciarczyk@gmail.com>
2021-04-06 22:48:30 +0100

    Add Car model to api application

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from rest_framework.exceptions import NotAcceptable
 
-# Create your models here.
+
+class Car(models.Model):
+    make = models.TextField()
+    model = models.TextField()
+    rating = models.FloatField(default=0)
+    rating_count = models.IntegerField(default=0)
+    url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/"
+
+    class Meta:
+        verbose_name = "Car"
+        verbose_name_plural = "Cars"
+
+    def __str__(self):
+        return self.url
+
+    def rate_me(self, rating):
+        if rating not in [1, 2, 3, 4, 5]:
+            raise NotAcceptable
+        else:
+            self.rating_count += 1
+            self.rating = (
+                self.rating * (self.rating_count - 1) + rating
+            ) / self.rating_count
